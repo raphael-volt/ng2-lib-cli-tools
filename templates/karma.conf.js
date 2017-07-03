9cfd3c49490606cf56569f2ab335eb0a5fdaac10
raphael_volt <raphael.ketmie@gmail.com>
2017-07-03 19:09:39 +0200

    moved templates

---
+++ templates/karma.conf.js
@@ -0,0 +1,33 @@
+// Karma configuration
+// Generated on Thu Jun 15 2017 15:21:49 GMT+0200 (CEST)
+
+module.exports = function (config) {
+  config.set({
+    basePath: './karma',
+    frameworks: ['jasmine', '@angular/cli'],
+    plugins: [
+      require('karma-jasmine'),
+      require('karma-chrome-launcher'),
+      require('karma-jasmine-html-reporter'),
+      require('karma-coverage-istanbul-reporter'),
+      require('@angular/cli/plugins/karma')
+    ],
+    client: {
+      clearContext: false // leave Jasmine Spec Runner output visible in browser
+    },
+    coverageIstanbulReporter: {
+      reports: ['html', 'lcovonly'],
+      fixWebpackSourcePaths: true
+    },
+    angularCli: {
+      environment: 'dev'
+    },
+    reporters: ['progress', 'kjhtml'],
+    port: 9876,
+    colors: true,
+    logLevel: config.LOG_INFO,
+    autoWatch: true,
+    browsers: ['Chrome'],
+    singleRun: false
+  })
+}
