70be66672f0c061643acbd1ab2e770c688730c72
raphael-volt <raphael@ketmie.com>
2017-07-13 00:57:04 +0200

    bugfix module import

--- templates/karma/main.ts
+++ templates/karma/main.ts
@@ -1,6 +1,6 @@
 import { enableProdMode } from '@angular/core';
 import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 
-import { {{moduleClass}} } from '../src/{{{moduleFilename}}}';
+import { {{moduleClass}} } from '../{{{moduleFilename}}}';
 
 platformBrowserDynamic().bootstrapModule({{moduleClass}});
